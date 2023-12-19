4acb530bf088fe40deade757369e3fe093042336
Park Junho <wns4711@naver.com>
2023-12-20 00:40:58 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.15 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+        int x = 2;
+        while(x <= n)
+        {
+            if(n % x == 1){
+             answer = x;   
+             break;
+            }
+
+            x++;
+            
+        }
+        
+        return answer;
+    }
+}
