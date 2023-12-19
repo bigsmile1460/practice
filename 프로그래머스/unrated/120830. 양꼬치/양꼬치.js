7b703f8abcc0a6a1907c869d61426146f299e9d2
Park Junho <wns4711@naver.com>
2023-12-20 00:33:35 +0900

    [level unrated] Title: 양꼬치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,11 @@
+function solution(n, k) {
+    var answer = 0;
+    if (n >= 10){
+        answer = n * 12000 + (k - parseInt(n / 10)) * 2000;
+    }
+    else{
+        answer = n * 12000 + k * 2000;
+    }
+
+    return answer;
+}
