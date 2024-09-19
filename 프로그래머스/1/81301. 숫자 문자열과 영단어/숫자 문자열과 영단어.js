ea6dbdd435313f3471c7d6ee1a24ef3621cca90b
Park Junho <wns4711@naver.com>
2024-09-19 09:49:42 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,12 @@
+
+
+function solution(s) {
+   const numberWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    numberWords.forEach((word, index) => {
+        const regex = new RegExp(word, 'g');
+        s = s.replace(regex, index);
+    });
+    
+    return parseInt(s);
+}
